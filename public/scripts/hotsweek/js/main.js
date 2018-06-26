7d35d1bb960d0edaf80caec43a0727c5d58eae03
Super <47396430@qq.com>
2018-06-26 16:07:36 +0800

    调整结构，修改接口URL

--- public/scripts/hotsweek/js/main.js
+++ public/scripts/hotsweek/js/main.js
@@ -1,12 +1,12 @@
 var presets
 var dataGlobal
 var dataPersonal
-var timestamp = Math.round(new Date().getTime() / 1000)
+var playerInfo
+// var timestamp = Math.round(new Date().getTime() / 1000)
 // var weekNumber = Math.trunc((timestamp + 345600) / 604800) + 1;
-var weekNumber = 2527
-var urlPresets = '/week/show/presets'
-var urlGlobal = '/week/show/personal/' + weekNumber + '/0'
-var urlPersonal = '/week/show/personal/' + weekNumber + '/46'
+var urlPresets = '/week/api/report/presets'
+var urlGlobal = '/week/api/report/global/' + weekNumber
+var urlPersonal = '/week/api/report/personal/' + weekNumber + '/' + playerID
 
 var fetchData = function (url, params = null) {
     return new Promise((resolve, reject) => {
@@ -42,11 +42,18 @@ Promise.all([
 }) {
     window.presets = presets
     window.dataGlobal = parseFields(dataGlobal)
+    window.playerInfo = dataPersonal.PlayerInfo
     window.dataPersonal = parseFields(dataPersonal)
+    console.log(dataGlobal)
+    console.log(dataPersonal)
     main()
 })
 
 var main = function () {
     // do someting
-    console.log(functions.weekWinRate[1]())
+    document.write('<h2>Player: ' + playerInfo.name + ' (from region ' + playerInfo.region + ')</h2>' + "<br />")
+    for (var i in functions) {
+        var item = functions[i]
+        document.write(item[0][lang] + ': ' + item[1]() + "<br />"); 
+    }
 }
